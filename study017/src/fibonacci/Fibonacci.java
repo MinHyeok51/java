94757e2253cc4c44abf2d7dd8c204b6da713349e
Minhyeok Lee <minhyeok51@naver.com>
2022-10-12 00:22:51 +0900

    java day17

---
+++ study017/src/fibonacci/Fibonacci.java
@@ -0,0 +1,74 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		//피보나치수열
+//		int a =0;
+//		int b= 1;
+//		int c=0;
+//		
+//		/*	System.out.println(a);
+//		System.out.println(b);	밑에 출력 순서만 바꿔주면 0,1 다 나옴*/
+//		for(int i=0; a+b<=1000; i++) {
+//			System.out.println(c);
+//			a=b;
+//			b=c;
+//			c = (a+b);
+//		}
+//
+//		while(c<=1000) {
+//			System.out.println(c);
+//			a=b;
+//			b=c;
+//			c=a+b;
+//		}
+//		for(c=a+b; a+b<=1000; c=a+b) {
+//			System.out.println(c);
+//			a=b;
+//			b=c;
+////			c = (a+b);
+//		}
+		
+		//----------------------------------------
+		/*근의공식 구하기
+		 * ax^2 + bx + c =0
+		 * */
+//		Scanner scan = new Scanner(System.in);
+//		int a =scan.nextInt();
+//		int b=scan.nextInt();
+//		int c=scan.nextInt();
+//		double root = Math.sqrt(b*b-4*a*c);
+//		double x1= (double)(-b +root)/ 2*a;
+//		double x2=(double)(-b -root)/ 2*a;
+//		System.out.println(x1);
+//		System.out.println(x2);
+		
+//		System.out.println(8%2);
+		
+		//----------------------------------------
+		
+//		int total=0;
+//		int i =1;
+//		while(i<=100) {
+//			total+=i;
+//			System.out.println(total);
+//			i++;
+//		}
+		
+//		for(int i=0; i<10; i++) {
+//				if(i==5) continue;
+//				System.out.println(i);
+//		}
+//		
+		int p;
+		
+		for(int i=4; i<=100; i++) {
+			for(int j=2; j<=100; j++) {
+//				if()
+			}
+		}
+	}
+}
